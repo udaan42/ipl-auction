8ecc4405c3c1619fd7e8f197f8bf436174291373
Hari Prasad <hariprasad.small@gmail.com>
2021-03-04 08:03:15 -0800

    Updated layout with dashboard

---
+++ client/components/league/League.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class League extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                League Page
+            </div>
+        )
+    }
+}
+
+export default League;
---
+++ client/containers/league/LeagueContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import League from '../../components/league/League';
+
+class LeagueContainer extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <League />
+        )
+    }
+}
+
+export default LeagueContainer;
