50428f8734e1183183382a5792f9c84a1c98009b
Hanumesh Muthuswamy <hanumeshm@hotmail.com>
2021-03-24 09:33:46 -0700

    initial redis commit

---
+++ server/config/redis.js
@@ -0,0 +1,25 @@
+const redis = require('redis');
+const { promisify } = require("util");
+require('dotenv').config();
+
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+});
+
+redisClient.on('connect', () => {
+    console.log('Redis client connected');
+});
+
+redisClient.on("error", (error) => {
+    console.error(error);
+});
+
+const get = promisify(redisClient.get).bind(redisClient);
+const set = promisify(redisClient.set).bind(redisClient);
+
+
+module.exports = {
+    get,
+    set
+};
