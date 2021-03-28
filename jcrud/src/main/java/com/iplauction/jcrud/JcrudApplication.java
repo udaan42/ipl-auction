e595f5df7f9ac516ab01c2ff25f4174618646079
PShanmug <pshanmuganathan@cccis.com>
2021-03-28 11:41:59 -0500

    Encryption Added

--- jcrud/src/main/java/com/iplauction/jcrud/JcrudApplication.java
+++ jcrud/src/main/java/com/iplauction/jcrud/JcrudApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -36,7 +37,7 @@ public class JcrudApplication {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 
 }
